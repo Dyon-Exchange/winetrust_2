77105c03b3d4c3eaa9df19253adc59e64d919b90
Steven Balagtas <steven@labrys.io>
2021-09-08 10:28:59 +1000

    chore: installed most of the basic koa packages

--- packages/koa-app/src/app.ts
+++ packages/koa-app/src/app.ts
@@ -1,9 +1,32 @@
+import cors from "@koa/cors";
 import Koa from "koa";
+import BodyParser from "koa-bodyparser";
 
-const app = new Koa();
+const app: Koa = new Koa();
+
+// cors setup
+app.use(cors());
+
+// body parser
+app.use(
+  BodyParser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+
+// logger
+app.use(async (ctx: Koa.Context, next: Function) => {
+  await next();
+});
 
 app.use(async (ctx) => {
   ctx.body = "Hello World";
+});
+
+// error-handling
+app.on("error", (err: Error, ctx: Koa.Context) => {
+  // eslint-disable-next-line no-console
+  console.error("server error", err, ctx);
 });
 
 app.listen(3000);
