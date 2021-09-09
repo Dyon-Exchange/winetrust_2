810573ef0eee22aa3cc988f71849aec683803ec7
Steven Balagtas <steven@labrys.io>
2021-09-10 09:36:10 +1000

    feat: setup material ui os level dark theme
    now adapts to os dark mode settings

--- packages/react-app/src/index.tsx
+++ packages/react-app/src/index.tsx
@@ -1,11 +1,32 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const AppThemeWrapper = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<AppThemeWrapper />, document.getElementById("root"));
