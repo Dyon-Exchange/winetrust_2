b14af47131fefc3b4d2fc1eeaf547e4e663241f1
Steven Balagtas <steven@labrys.io>
2021-09-10 17:33:57 +1000

    feat: implemented react hook form validation in the login page

--- packages/react-app/src/components/organisms/authentication/LoginForm.tsx
+++ packages/react-app/src/components/organisms/authentication/LoginForm.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
+  IconButton,
   Input,
   InputGroup,
   InputRightElement,
@@ -12,6 +15,8 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import React, { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import isEmail from "validator/lib/isEmail";
 
 // returns the icon shown on the toggle reveal button
 const ToggleRevealPasswordIcon = ({ reveal }: { reveal: boolean }) => {
@@ -24,7 +29,7 @@ const LoginForm = () => {
   const themeBackgroundColor = useColorModeValue("white", "gray.600");
 
   // state to reveal or hide password
-  const [reveal, setReveal] = useBoolean();
+  const [reveal, setReveal] = useBoolean(false);
 
   // returns the input type for the password field depending on `reveal``
   const passwordInputType: "text" | "password" = useMemo(() => {
@@ -32,8 +37,25 @@ const LoginForm = () => {
     return "password";
   }, [reveal]);
 
+  // aria label for the password input reveal button
+  const passwordRevealButtonAriaLabel: "Hide password" | "Reveal password" =
+    useMemo(() => {
+      if (reveal) return "Hide password";
+      return "Reveal password";
+    }, [reveal]);
+
   // function to toggle revealing password
   const toggleRevealPassword = () => setReveal.toggle();
+
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
+
+  // submit handler
+  const onSubmit = (data: LoginForm) => {};
 
   return (
     <Box
@@ -43,26 +65,56 @@ const LoginForm = () => {
       p="50px"
       minW="400px"
     >
-      <VStack spacing="25px">
-        <FormControl id="email">
-          <FormLabel fontSize="sm">Email address</FormLabel>
-          <Input fontSize="sm" type="email" />
-        </FormControl>
-        <FormControl id="password">
-          <FormLabel fontSize="sm">Password</FormLabel>
-          <InputGroup>
-            <Input fontSize="sm" type={passwordInputType} />
-            <InputRightElement>
-              <Button size="sm" onClick={toggleRevealPassword}>
-                <ToggleRevealPasswordIcon reveal={reveal} />
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormControl>
-        <Button colorScheme="blue" isFullWidth>
-          Sign in
-        </Button>
-      </VStack>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <VStack spacing="25px">
+          <FormControl id="email">
+            <FormLabel fontSize="sm">Email address</FormLabel>
+            <Input
+              {...register("email", {
+                required: "Email address is required",
+                validate: (email?: string) =>
+                  isEmail(email || "") ? undefined : "Invalid email address",
+              })}
+              fontSize="sm"
+              type="email"
+              isInvalid={errors.email !== undefined}
+            />
+            {errors.email !== undefined && (
+              <FormHelperText color="red.500" fontSize="sm">
+                {errors.email.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel fontSize="sm">Password</FormLabel>
+            <InputGroup>
+              <Input
+                {...register("password", { required: "Password is required" })}
+                fontSize="sm"
+                type={passwordInputType}
+                isInvalid={errors.password !== undefined}
+              />
+              <InputRightElement>
+                <IconButton
+                  aria-label={passwordRevealButtonAriaLabel}
+                  bg="transparent"
+                  size="sm"
+                  icon={<ToggleRevealPasswordIcon reveal={reveal} />}
+                  onClick={toggleRevealPassword}
+                />
+              </InputRightElement>
+            </InputGroup>
+            {errors.password !== undefined && (
+              <FormHelperText color="red.500" fontSize="sm">
+                {errors.password.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+          <Button colorScheme="blue" isFullWidth type="submit">
+            Sign in
+          </Button>
+        </VStack>
+      </form>
     </Box>
   );
 };
--- packages/react-app/src/pages/auth/Login.tsx
+++ packages/react-app/src/pages/auth/Login.tsx
@@ -5,8 +5,8 @@ import LoginForm from "../../components/organisms/authentication/LoginForm";
 
 const Login = () => (
   <Center py="100px">
-    <VStack spacing="100px">
-      <Heading fontSize="2xl">Sign in to your account</Heading>
+    <VStack spacing="50px">
+      <Heading fontSize="3xl">Sign in to your account</Heading>
       <LoginForm />
     </VStack>
   </Center>
