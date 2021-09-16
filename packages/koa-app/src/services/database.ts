281948a0c74d71f9d885e0f2ac557b251d2b96bc
Steven Balagtas <steven@labrys.io>
2021-09-17 09:49:19 +1000

    fix: fix koa app not connecting to the mongo db cluster

--- packages/koa-app/src/services/database.ts
+++ packages/koa-app/src/services/database.ts
@@ -3,7 +3,8 @@ import { mongoose } from "@typegoose/typegoose";
 
 const connect = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.LOCAL_MONGO_DB_CONNECTION_URL);
+    // await mongoose.connect(process.env.LOCAL_MONGO_DB_CONNECTION_URL);
+    await mongoose.connect(process.env.MONGO_DB_CONNECTION_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB");
