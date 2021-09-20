81e47f857006bbe3ffb8b9090b0027d2bba2388d
Steven Balagtas <steven@labrys.io>
2021-09-20 10:44:08 +1000

    feat: added metamask brand icons
    added metamask icon to the isntall metamask modal

--- packages/react-app/src/components/organisms/authentication/InstallMetaMaskModal.tsx
+++ packages/react-app/src/components/organisms/authentication/InstallMetaMaskModal.tsx
@@ -1,15 +1,20 @@
 import {
   Button,
+  Image,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalBody,
   ModalFooter,
+  Text,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import MetaMaskOnboarding from "@metamask/onboarding";
 import React from "react";
+
+import MetaMaskHorizontal from "../../../assets/icons/metamask/metamask-fox-wordmark-horizontal.svg";
 
 const InstallMetaMaskModal = () => {
   const isMetaMaskInstalled = window.ethereum;
@@ -34,7 +39,17 @@ const InstallMetaMaskModal = () => {
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Please install MetaMask</ModalHeader>
-        <ModalBody>You need an Ethereum wallet to use WineTrust.</ModalBody>
+        <ModalBody>
+          <VStack>
+            <Image
+              alt="MetaMask fox wordmark horizontal"
+              alignContent="center"
+              src={MetaMaskHorizontal}
+              w="60%"
+            />
+            <Text>You need an Ethereum wallet to use WineTrust.</Text>
+          </VStack>
+        </ModalBody>
         <ModalFooter justifyContent="center">
           <Button colorScheme="blue" onClick={installMetaMask}>
             Get MetaMask
