178e02f5016bf2a1642aabf3c99a4ea4d0028ac1
Steven Balagtas <steven@labrys.io>
2021-09-22 14:20:23 +1000

    feat: implemented add new warehouse form modal

---
+++ packages/react-app/src/components/organisms/data/warehouses/AddNewWarehouseFormModal.tsx
@@ -0,0 +1,157 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import React from "react";
+import { useForm } from "react-hook-form";
+import isEmail from "validator/lib/isEmail";
+
+import useThemeColors from "../../../../hooks/theme/useThemeColors";
+
+const StyledButton = styled(Button)`
+  margin: 10px 5px;
+  width: 100px;
+`;
+
+const StyledFormControl = styled(FormControl)`
+  margin: 15px 0px;
+`;
+
+interface AddNewWareHouseFormModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AddNewWarehouseFormModal = ({
+  isOpen,
+  onClose,
+}: AddNewWareHouseFormModalProps) => {
+  // get theme colors
+  const colors = useThemeColors();
+
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isSubmitting },
+  } = useForm<NewWarehouseForm>();
+
+  // submit handler
+  const onSubmit = async (data: NewWarehouseForm) => {
+    // TODO: implement request to create new warehouse
+  };
+
+  return (
+    <Modal
+      closeOnEsc={!isDirty}
+      closeOnOverlayClick={!isDirty}
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+      size="xl"
+    >
+      <ModalOverlay />
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <ModalContent>
+          <ModalHeader>Add New Warehouse</ModalHeader>
+          <ModalBody alignSelf="center" w="80%">
+            <StyledFormControl id="warehouseName" isDisabled={isSubmitting}>
+              <FormLabel fontSize="sm">Warehouse name</FormLabel>
+              <Input
+                {...register("warehouseName", {
+                  required: "Warehouse name is required",
+                })}
+                fontSize="sm"
+                type="text"
+                isInvalid={errors.warehouseName !== undefined}
+              />
+              {errors.warehouseName !== undefined && (
+                <FormHelperText color={colors.error} fontSize="sm">
+                  {errors.warehouseName.message}
+                </FormHelperText>
+              )}
+            </StyledFormControl>
+
+            <StyledFormControl id="warehouseAddress" isDisabled={isSubmitting}>
+              <FormLabel fontSize="sm">Warehouse address</FormLabel>
+              <Input
+                {...register("warehouseAddress", {
+                  required: "Warehouse address is required",
+                })}
+                fontSize="sm"
+                type="text"
+                isInvalid={errors.warehouseAddress !== undefined}
+              />
+              {errors.warehouseAddress !== undefined && (
+                <FormHelperText color={colors.error} fontSize="sm">
+                  {errors.warehouseAddress.message}
+                </FormHelperText>
+              )}
+            </StyledFormControl>
+
+            <StyledFormControl id="contactName" isDisabled={isSubmitting}>
+              <FormLabel fontSize="sm">Contact name</FormLabel>
+              <Input
+                {...register("contactName", {
+                  required: "Contact name is required",
+                })}
+                fontSize="sm"
+                type="text"
+                isInvalid={errors.contactName !== undefined}
+              />
+              {errors.contactName !== undefined && (
+                <FormHelperText color={colors.error} fontSize="sm">
+                  {errors.contactName.message}
+                </FormHelperText>
+              )}
+            </StyledFormControl>
+
+            <StyledFormControl id="contactEmail" isDisabled={isSubmitting}>
+              <FormLabel fontSize="sm">Contact email address</FormLabel>
+              <Input
+                {...register("contactEmail", {
+                  required: "Contact email address is required",
+                  validate: (email?: string) =>
+                    isEmail(email || "") ? undefined : "Invalid email address",
+                })}
+                fontSize="sm"
+                type="email"
+                isInvalid={errors.contactEmail !== undefined}
+              />
+              {errors.contactEmail !== undefined && (
+                <FormHelperText color={colors.error} fontSize="sm">
+                  {errors.contactEmail.message}
+                </FormHelperText>
+              )}
+            </StyledFormControl>
+          </ModalBody>
+          <ModalFooter>
+            <StyledButton colorScheme="blue" type="submit">
+              Add
+            </StyledButton>
+            <StyledButton
+              colorScheme="blue"
+              onClick={onClose}
+              variant="outline"
+            >
+              Cancel
+            </StyledButton>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddNewWarehouseFormModal;
