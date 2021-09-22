33f07204b999cbd4cef6fadadd532451f4ad9764
Steven Balagtas <steven@labrys.io>
2021-09-23 09:24:41 +1000

    feat: made the tabs in the data tab controlled and now opens the appropriate add new modal

--- packages/react-app/src/components/organisms/data/WineTrustData.tsx
+++ packages/react-app/src/components/organisms/data/WineTrustData.tsx
@@ -6,7 +6,7 @@ import {
   Tabs,
   useDisclosure,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 import useThemeColors from "../../../hooks/theme/useThemeColors";
 import AddNewButton from "../../atoms/buttons/AddNewButton";
@@ -15,6 +15,9 @@ import AddNewWarehouseFormModal from "./warehouses/AddNewWarehouseFormModal";
 
 const WineTrustData = () => {
   const colors = useThemeColors();
+
+  // state for the tab index
+  const [tabIndex, setTabIndex] = useState(0);
 
   // states for the add new modals
   const {
@@ -25,17 +28,28 @@ const WineTrustData = () => {
     defaultIsOpen: false,
   });
 
+  // tab change handler
+  const handleTabChange = (index: number) => {
+    setTabIndex(index);
+  };
+
+  // open add new handler
+  const openAddNew = () => {
+    // open the appropriate add new modal
+    if (tabIndex === 0) openAddNewWarehouse();
+  };
+
   return (
     <>
       <Box bg={colors.secondary} boxShadow="sm" flex="1">
-        <Tabs>
+        <Tabs index={tabIndex} onChange={handleTabChange}>
           <HStack justifyContent="space-between" p="10px 20px" w="100%">
             <TabList>
               <Tab>Warehouses</Tab>
               <Tab>Clients</Tab>
               <Tab>Products</Tab>
             </TabList>
-            <AddNewButton onClick={openAddNewWarehouse} />
+            <AddNewButton onClick={openAddNew} />
           </HStack>
         </Tabs>
       </Box>
