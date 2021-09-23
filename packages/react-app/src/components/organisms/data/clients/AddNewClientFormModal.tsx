b066702502ae4b898ac522740037c298ec3c0a67
Steven Balagtas <steven@labrys.io>
2021-09-23 13:50:50 +1000

    feat: implemented phone number input

--- packages/react-app/src/components/organisms/data/clients/AddNewClientFormModal.tsx
+++ packages/react-app/src/components/organisms/data/clients/AddNewClientFormModal.tsx
@@ -3,16 +3,21 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  InputGroup,
+  InputLeftAddon,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Select,
 } from "@chakra-ui/react";
+import CountryData from "country-data";
 import React from "react";
 import { useForm } from "react-hook-form";
 import isEthereumAddress from "validator/lib/isEthereumAddress";
+import isMobilePhone from "validator/lib/isMobilePhone";
 
 import useThemeColors from "../../../../hooks/theme/useThemeColors";
 import ModalFooterButton from "../../../atoms/buttons/ModalFooterButton";
@@ -100,6 +105,54 @@ const AddNewClientFormModal = ({
                   {errors.ethAddress.message}
                 </FormHelperText>
               )}
+            </ModalFormControl>
+
+            <ModalFormControl id="phoneNumber" isDisabled={isSubmitting}>
+              <FormLabel fontSize="sm">Phone number</FormLabel>
+              <InputGroup>
+                <InputLeftAddon>
+                  <Select
+                    {...register("phoneNumber.countryCode", {
+                      required: "Country code is required",
+                    })}
+                    fontSize="sm"
+                    placeholder="Select"
+                    variant="flushed"
+                    isInvalid={errors.phoneNumber?.countryCode !== undefined}
+                  >
+                    {CountryData.callingCountries.all.map((country) => (
+                      <option
+                        key={country.name}
+                        value={country.countryCallingCodes[0]}
+                        label={country.countryCallingCodes[0]}
+                      />
+                    ))}
+                  </Select>
+                </InputLeftAddon>
+                <Input
+                  {...register("phoneNumber.phoneNumber", {
+                    required: "Phone number is required",
+                    validate: (phoneNumber: string) =>
+                      isMobilePhone(phoneNumber)
+                        ? undefined
+                        : "Invalid phone number",
+                  })}
+                  fontSize="sm"
+                  type="text"
+                  isInvalid={errors.phoneNumber !== undefined}
+                />
+              </InputGroup>
+              {errors.phoneNumber?.countryCode !== undefined && (
+                <FormHelperText color={colors.error} fontSize="sm">
+                  {errors.phoneNumber.countryCode.message}
+                </FormHelperText>
+              )}
+              {errors.phoneNumber?.phoneNumber !== undefined &&
+                !errors.phoneNumber?.countryCode && (
+                  <FormHelperText color={colors.error} fontSize="sm">
+                    {errors.phoneNumber.phoneNumber.message}
+                  </FormHelperText>
+                )}
             </ModalFormControl>
           </ModalBody>
           <ModalFooter>
