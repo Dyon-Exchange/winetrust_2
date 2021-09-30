d8953496f77577c436e163916effcf8d2d877bb1
Steven Balagtas <steven@labrys.io>
2021-09-30 13:28:29 +1000

    feat: implemented data grid in the products tab
    need to invalidate cache when creating a new product

--- packages/react-app/src/components/organisms/data/products/ProductsTable.tsx
+++ packages/react-app/src/components/organisms/data/products/ProductsTable.tsx
@@ -1,11 +1,37 @@
 import { useToast } from "@chakra-ui/react";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { AxiosError } from "axios";
+import { orderBy } from "lodash";
 import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 
 import getProducts from "../../../../api/data/products/getProducts";
+import StyledDataGrid from "../../../atoms/tables/StyledDataGrid";
 import DataTableError from "../../../molecules/dataTables/DataTableError";
 import DataTableSpinner from "../../../molecules/dataTables/DataTableSpinner";
+
+// column headers for the products data table
+const productsTableColumns: GridColDef[] = [
+  { field: "productName", headerName: "Product Name", flex: 1, minWidth: 200 },
+  { field: "year", headerName: "Year", flex: 1, minWidth: 125 },
+  { field: "region", headerName: "Region", flex: 1, minWidth: 200 },
+  {
+    field: "subRegion",
+    headerName: "Sub-Region",
+    flex: 1,
+    minWidth: 200,
+    valueGetter: (param: GridValueGetterParams) => param.value || "--",
+  },
+  {
+    field: "subSubRegion",
+    headerName: "Sub-Sub-Region",
+    flex: 1,
+    minWidth: 200,
+    valueGetter: (param: GridValueGetterParams) => param.value || "--",
+  },
+  { field: "packSize", headerName: "Pack Size", flex: 1, minWidth: 150 },
+  { field: "dutyStatus", headerName: "Duty Status", flex: 1, minWidth: 150 },
+];
 
 const ProductsTable = () => {
   const toast = useToast();
@@ -45,7 +71,16 @@ const ProductsTable = () => {
       />
     );
 
-  return <></>;
+  return (
+    <StyledDataGrid
+      autoHeight
+      disableSelectionOnClick
+      disableColumnSelector
+      hideFooter
+      columns={productsTableColumns}
+      rows={orderBy(productsData, "createdAt", "desc") ?? []}
+    />
+  );
 };
 
 export default ProductsTable;
