b6f8c11d65f60bb1ec3c583f85b3d31d84a4eda1
Steven Balagtas <steven@labrys.io>
2021-10-06 16:01:25 +1000

    feat: hooked up react hook forms to the add new pre advice form modal
    need to implement warehouse selects

--- packages/react-app/src/components/organisms/preAdvice/AddNewPreAdviceFormModal.tsx
+++ packages/react-app/src/components/organisms/preAdvice/AddNewPreAdviceFormModal.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import {
+  FormErrorMessage,
   FormLabel,
   Input,
   Modal,
@@ -11,12 +13,18 @@ import {
 } from "@chakra-ui/react";
 import { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 
 import searchClients from "../../../api/data/clients/searchClients";
 import ModalFooterButton from "../../atoms/buttons/ModalFooterButton";
 import ModalFormControl from "../../atoms/forms/ModalFormControl";
 import StyledChakraReactSelect from "../../atoms/inputs/StyledChakraReactSelect";
+
+interface SelectedClientOption {
+  label: string;
+  value: Client;
+}
 
 interface AddNewPreAdviceFormModalProps {
   isOpen: boolean;
@@ -59,46 +67,89 @@ const AddNewPreAdviceFormModal = ({
     }
   }, [clientsError, clientsIsError, toast]);
 
+  // react hook form
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isSubmitting },
+  } = useForm<NewPreAdviceForm>();
+
+  // submit handler
+  const onSubmit = (data: NewPreAdviceForm) => {};
+
   // close modal handler
   const closeModal = () => onClose();
 
   return (
-    <Modal isCentered isOpen={isOpen} onClose={closeModal} size="xl">
+    <Modal
+      closeOnEsc={!isDirty}
+      closeOnOverlayClick={!isDirty}
+      isCentered
+      isOpen={isOpen}
+      onClose={closeModal}
+      size="xl"
+    >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Add New Pre-Advice</ModalHeader>
-        <ModalBody alignSelf="center" w="80%">
-          {/* Implement add new pre-advice form here... */}
-          <ModalFormControl>
-            <FormLabel fontSize="sm">Owner</FormLabel>
-            <StyledChakraReactSelect
-              isLoading={clientsDataIsFetching}
-              placeholder="Search client"
-              onInputChange={(search: string) => setClientSearchQuery(search)}
-              options={clientsData?.map((client: Client) => ({
-                value: client,
-                label: `${client.firstName} ${client.lastName}`,
-              }))}
-            />
-          </ModalFormControl>
-          <ModalFormControl>
-            <FormLabel fontSize="sm">Test</FormLabel>
-            <Input fontSize="sm" placeholder="Placeholder" />
-          </ModalFormControl>
-        </ModalBody>
-        <ModalFooter>
-          <ModalFooterButton colorScheme="blue" type="submit">
-            Add
-          </ModalFooterButton>
-          <ModalFooterButton
-            colorScheme="blue"
-            onClick={closeModal}
-            variant="outline"
-          >
-            Cancel
-          </ModalFooterButton>
-        </ModalFooter>
-      </ModalContent>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <ModalContent>
+          <ModalHeader>Add New Pre-Advice</ModalHeader>
+          <ModalBody alignSelf="center" w="80%">
+            <ModalFormControl isInvalid={errors.owner !== undefined}>
+              <FormLabel fontSize="sm">Owner</FormLabel>
+              <Controller
+                control={control}
+                name="owner"
+                rules={{ required: "Owner is required." }}
+                render={({ field: { onChange } }) => (
+                  <StyledChakraReactSelect
+                    isLoading={clientsDataIsFetching}
+                    placeholder="Search client"
+                    onChange={(selectedClient) => {
+                      const client = (selectedClient as SelectedClientOption)
+                        ?.value;
+                      if (client) {
+                        onChange(client);
+                      } else {
+                        onChange(undefined);
+                      }
+                    }}
+                    onInputChange={(search: string) =>
+                      setClientSearchQuery(search)
+                    }
+                    options={clientsData?.map((client: Client) => ({
+                      value: client,
+                      label: `${client.firstName} ${client.lastName}`,
+                    }))}
+                  />
+                )}
+              />
+              {errors.owner !== undefined && (
+                <FormErrorMessage fontSize="sm">
+                  {/* typescript being buggy, thinks errors.owner is a client type */}
+                  {(errors.owner as any).message}
+                </FormErrorMessage>
+              )}
+            </ModalFormControl>
+            <ModalFormControl>
+              <FormLabel fontSize="sm">Test</FormLabel>
+              <Input fontSize="sm" placeholder="Placeholder" />
+            </ModalFormControl>
+          </ModalBody>
+          <ModalFooter>
+            <ModalFooterButton colorScheme="blue" type="submit">
+              Add
+            </ModalFooterButton>
+            <ModalFooterButton
+              colorScheme="blue"
+              onClick={closeModal}
+              variant="outline"
+            >
+              Cancel
+            </ModalFooterButton>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
---
+++ packages/react-app/src/types/preAdvice/PreAdvice.d.ts
@@ -0,0 +1,5 @@
+interface NewPreAdviceForm {
+  owner: Client;
+  transferringWarehouse: Warehouse;
+  arrivalWarehouse: Warehouse;
+}
