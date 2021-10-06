491af64cf3e60bbf083d504929ea5e653eff709c
Steven Balagtas <steven@labrys.io>
2021-10-06 11:33:18 +1000

    feat: got basic chakra react select working finally

--- packages/react-app/src/components/organisms/preAdvice/AddNewPreAdviceFormModal.tsx
+++ packages/react-app/src/components/organisms/preAdvice/AddNewPreAdviceFormModal.tsx
@@ -1,4 +1,6 @@
 import {
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalContent,
@@ -6,9 +8,11 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
+import { AsyncSelect } from "chakra-react-select";
 import React from "react";
 
 import ModalFooterButton from "../../atoms/buttons/ModalFooterButton";
+import ModalFormControl from "../../atoms/forms/ModalFormControl";
 
 interface AddNewPreAdviceFormModalProps {
   isOpen: boolean;
@@ -28,7 +32,26 @@ const AddNewPreAdviceFormModal = ({
       <ModalContent>
         <ModalHeader>Add New Pre-Advice</ModalHeader>
         <ModalBody alignSelf="center" w="80%">
-          Implement add new pre-advice form here...
+          {/* Implement add new pre-advice form here... */}
+          <ModalFormControl>
+            <FormLabel fontSize="sm">Owner</FormLabel>
+            <AsyncSelect
+              placeholder="Search client"
+              styles={{
+                input: () => ({
+                  fontSize: "14px",
+                }),
+                placeholder: (base) => ({
+                  ...base,
+                  fontSize: "14px",
+                }),
+              }}
+            />
+          </ModalFormControl>
+          <ModalFormControl>
+            <FormLabel fontSize="sm">Test</FormLabel>
+            <Input fontSize="sm" placeholder="Placeholder" />
+          </ModalFormControl>
         </ModalBody>
         <ModalFooter>
           <ModalFooterButton colorScheme="blue" type="submit">
