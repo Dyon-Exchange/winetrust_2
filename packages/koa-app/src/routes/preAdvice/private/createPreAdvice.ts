f7749291d3439db686db5625419bcd24a8814428
Steven Balagtas <steven@labrys.io>
2021-10-08 15:48:28 +1000

    feat: implemented sessions in the create pre advice route

--- packages/koa-app/src/routes/preAdvice/private/createPreAdvice.ts
+++ packages/koa-app/src/routes/preAdvice/private/createPreAdvice.ts
@@ -1,6 +1,8 @@
 import { Request } from "koa";
+import mongoose from "mongoose";
 
 import Client from "../../../models/Client";
+import PreAdvice from "../../../models/PreAdvice";
 import Warehouse from "../../../models/Warehouse";
 import ExtendedContext from "../../../types/koa/ExtendedContext";
 
@@ -17,26 +19,52 @@ export default async (ctx: ExtendedContext<CreatePreAdviceRequest>) => {
     ctx.request.body;
 
   // get the owner, transferring warehouse and arrival warehouse documents and check they exist
-  const owner = await Client.find({ _id: ownerId });
+  const owner = await Client.findById({ _id: ownerId });
   if (!owner) {
     ctx.throw(400, "No client found");
   }
 
-  const transferringWarehouse = await Warehouse.find({
+  const transferringWarehouse = await Warehouse.findById({
     _id: transferringWarehouseId,
   });
   if (!transferringWarehouse) {
     ctx.throw(400, "No transferring warehouse found");
   }
 
-  const arrivalWarehouse = await Warehouse.find({ _id: arrivalWarehouseId });
+  const arrivalWarehouse = await Warehouse.findById({
+    _id: arrivalWarehouseId,
+  });
   if (!arrivalWarehouse) {
     ctx.throw(400, "No arrival warehouse found");
   }
 
-  console.log({ owner });
-  console.log({ transferringWarehouse });
-  console.log({ arrivalWarehouse });
+  // start a mongoose session, used to revert the creation of the pre-advice and any assets if any error occurs
+  const session = await mongoose.startSession();
+  try {
+    // start the session transaction
+    session.startTransaction();
+
+    // create the pre advice
+    const newPreAdvice = await PreAdvice.create(
+      [
+        {
+          owner,
+          transferringWarehouse,
+          arrivalWarehouse,
+        },
+      ],
+      { session }
+    );
+
+    // commit and end the session
+    await session.commitTransaction();
+    session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    ctx.throw(500, "Error creating this pre advice");
+  }
 
   ctx.status = 200;
 };
