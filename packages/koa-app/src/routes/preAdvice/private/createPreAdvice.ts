939bf96d3157b6bdaf4b6b13a3a847093f45e736
Steven Balagtas <steven@labrys.io>
2021-10-11 10:44:45 +1000

    feat: finished create pre advice route logic
    need to make the front end flow smoother

--- packages/koa-app/src/routes/preAdvice/private/createPreAdvice.ts
+++ packages/koa-app/src/routes/preAdvice/private/createPreAdvice.ts
@@ -1,8 +1,10 @@
 import { Request } from "koa";
 import mongoose from "mongoose";
 
+import Asset from "../../../models/Asset";
 import Client from "../../../models/Client";
 import PreAdvice from "../../../models/PreAdvice";
+import Product from "../../../models/Product";
 import Warehouse from "../../../models/Warehouse";
 import ExtendedContext from "../../../types/koa/ExtendedContext";
 
@@ -30,21 +32,19 @@ export default async (ctx: ExtendedContext<CreatePreAdviceRequest>) => {
     ctx.request.body;
 
   // get the owner, transferring warehouse and arrival warehouse documents and check they exist
-  const owner = await Client.findById({ _id: ownerId });
+  const owner = await Client.findById(ownerId);
   if (!owner) {
     ctx.throw(400, "No client found");
   }
 
-  const transferringWarehouse = await Warehouse.findById({
-    _id: transferringWarehouseId,
-  });
+  const transferringWarehouse = await Warehouse.findById(
+    transferringWarehouseId
+  );
   if (!transferringWarehouse) {
     ctx.throw(400, "No transferring warehouse found");
   }
 
-  const arrivalWarehouse = await Warehouse.findById({
-    _id: arrivalWarehouseId,
-  });
+  const arrivalWarehouse = await Warehouse.findById(arrivalWarehouseId);
   if (!arrivalWarehouse) {
     ctx.throw(400, "No arrival warehouse found");
   }
@@ -55,19 +55,42 @@ export default async (ctx: ExtendedContext<CreatePreAdviceRequest>) => {
     // start the session transaction
     session.startTransaction();
 
-    console.log(assets);
+    // create the pre advice, will return an array, pull the pre advice object out of the array
+    const [preAdvice] = await PreAdvice.create(
+      [
+        {
+          owner,
+          transferringWarehouse,
+          arrivalWarehouse,
+        },
+      ],
+      { session }
+    );
 
-    // // create the pre advice
-    // const newPreAdvice = await PreAdvice.create(
-    //   [
-    //     {
-    //       owner,
-    //       transferringWarehouse,
-    //       arrivalWarehouse,
-    //     },
-    //   ],
-    //   { session }
-    // );
+    // loop through assets and create the assets linking them to this pre advice
+    await Promise.all(
+      assets.map(async (asset) => {
+        // get the product for this asset
+        const product = await Product.findById(asset.productId, null, {
+          session,
+        });
+
+        if (!product) {
+          throw new Error("No product found");
+        }
+
+        // create an array with length asset.quantity of the asset
+        const formattedAssets = new Array(asset.quantity).fill(0).map(() => ({
+          preAdvice,
+          product,
+          cost: asset.cost,
+          expectedArrivalDate: asset.expectedArrivalDate,
+        }));
+
+        // create the asset/s in the db
+        await Asset.insertMany(formattedAssets, { session });
+      })
+    );
 
     // commit and end the session
     await session.commitTransaction();
@@ -75,7 +98,6 @@ export default async (ctx: ExtendedContext<CreatePreAdviceRequest>) => {
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
-
     ctx.throw(500, "Error creating this pre advice");
   }
 
