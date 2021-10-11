c6ed5a146786a4869c7be20135a8e79da71bf22c
Steven Balagtas <steven@labrys.io>
2021-10-12 09:49:29 +1000

    feat: pr review comments, added confirm cancel modal to the add new asset modal

--- packages/react-app/src/components/organisms/preAdvice/AddNewAssetFormModal.tsx
+++ packages/react-app/src/components/organisms/preAdvice/AddNewAssetFormModal.tsx
@@ -14,6 +14,7 @@ import {
   NumberInput,
   NumberInputField,
   Select,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 import { AxiosError } from "axios";
@@ -28,6 +29,7 @@ import supportedCurrencies from "../../../constants/supportedCurrencies";
 import ModalFooterButton from "../../atoms/buttons/ModalFooterButton";
 import ModalFormControl from "../../atoms/forms/ModalFormControl";
 import StyledChakraReactSelect from "../../atoms/inputs/StyledChakraReactSelect";
+import ConfirmCancelChangesModal from "../../molecules/modals/ConfirmCancelChangesModal";
 
 // extend dayjs with the isSameOrAfter plugin
 dayjs.extend(isSameOrAfter);
@@ -94,158 +96,175 @@ const AddNewAssetFormModal = ({
     onClose();
   };
 
+  // state for the confirm cancel modal
+  const {
+    isOpen: isConfirmCancelModalOpen,
+    onOpen: openConfirmCancel,
+    onClose: closeConfirmCancel,
+  } = useDisclosure({
+    defaultIsOpen: false,
+  });
+
   // close modal handler
-  const closeModal = () => onClose();
+  const closeModal = () => (isDirty ? openConfirmCancel() : onClose());
 
   return (
-    <Modal
-      closeOnEsc={!isDirty}
-      closeOnOverlayClick={!isDirty}
-      isOpen={isOpen}
-      onClose={onClose}
-      scrollBehavior="inside"
-    >
-      <ModalOverlay />
-      <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <ModalContent>
-          <ModalHeader>Add New Asset</ModalHeader>
-          <ModalBody alignSelf="center" w="80%">
-            <ModalFormControl isInvalid={errors.product !== undefined}>
-              <FormLabel fontSize="sm">Product</FormLabel>
-              <Controller
-                control={control}
-                name="product"
-                rules={{ required: "Product is required." }}
-                render={({ field: { onChange } }) => (
-                  <StyledChakraReactSelect
-                    isLoading={productsDataIsFetching}
-                    placeholder="Search product"
-                    onChange={(selectedClient) => {
-                      const product = (selectedClient as SelectedProductOption)
-                        ?.value;
-                      if (product) {
-                        onChange(product);
-                      } else {
-                        onChange(undefined);
+    <>
+      <Modal
+        closeOnEsc={!isDirty}
+        closeOnOverlayClick={!isDirty}
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <ModalContent>
+            <ModalHeader>Add New Asset</ModalHeader>
+            <ModalBody alignSelf="center" w="80%">
+              <ModalFormControl isInvalid={errors.product !== undefined}>
+                <FormLabel fontSize="sm">Product</FormLabel>
+                <Controller
+                  control={control}
+                  name="product"
+                  rules={{ required: "Product is required." }}
+                  render={({ field: { onChange } }) => (
+                    <StyledChakraReactSelect
+                      isLoading={productsDataIsFetching}
+                      placeholder="Search product"
+                      onChange={(selectedClient) => {
+                        const product = (
+                          selectedClient as SelectedProductOption
+                        )?.value;
+                        if (product) {
+                          onChange(product);
+                        } else {
+                          onChange(undefined);
+                        }
+                      }}
+                      onInputChange={(search: string) =>
+                        setProductSearchQuery(search)
                       }
-                    }}
-                    onInputChange={(search: string) =>
-                      setProductSearchQuery(search)
-                    }
-                    options={productsData?.map((product: Product) => ({
-                      value: product,
-                      label: product.productName,
-                    }))}
-                  />
+                      options={productsData?.map((product: Product) => ({
+                        value: product,
+                        label: product.productName,
+                      }))}
+                    />
+                  )}
+                />
+                {errors.product !== undefined && (
+                  <FormErrorMessage fontSize="sm">
+                    {/* typescript being buggy, thinks errors.product is a product type */}
+                    {(errors.product as any).message}
+                  </FormErrorMessage>
                 )}
-              />
-              {errors.product !== undefined && (
-                <FormErrorMessage fontSize="sm">
-                  {/* typescript being buggy, thinks errors.product is a product type */}
-                  {(errors.product as any).message}
-                </FormErrorMessage>
-              )}
-            </ModalFormControl>
+              </ModalFormControl>
 
-            <ModalFormControl isInvalid={errors.costPrice !== undefined}>
-              <FormLabel fontSize="sm">Cost price</FormLabel>
-              <InputGroup>
-                <InputLeftAddon>
-                  <Select
-                    {...register("costPrice.currency", {
-                      required: "Currency is required",
-                    })}
-                    fontSize="sm"
-                    placeholder="Select"
-                    variant="flushed"
-                    isInvalid={errors.costPrice?.currency !== undefined}
-                  >
-                    {supportedCurrencies.map((country) => (
-                      <option
-                        key={country.name}
-                        value={country.code}
-                        label={`${country.code} (${country.symbol})`}
-                      />
-                    ))}
-                  </Select>
-                </InputLeftAddon>
-                <NumberInput min={0} w="100%">
+              <ModalFormControl isInvalid={errors.costPrice !== undefined}>
+                <FormLabel fontSize="sm">Cost price</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon>
+                    <Select
+                      {...register("costPrice.currency", {
+                        required: "Currency is required",
+                      })}
+                      fontSize="sm"
+                      placeholder="Select"
+                      variant="flushed"
+                      isInvalid={errors.costPrice?.currency !== undefined}
+                    >
+                      {supportedCurrencies.map((country) => (
+                        <option
+                          key={country.name}
+                          value={country.code}
+                          label={`${country.code} (${country.symbol})`}
+                        />
+                      ))}
+                    </Select>
+                  </InputLeftAddon>
+                  <NumberInput min={0} w="100%">
+                    <NumberInputField
+                      {...register("costPrice.amount", {
+                        required: "Cost price is required",
+                      })}
+                      fontSize="sm"
+                    />
+                  </NumberInput>
+                </InputGroup>
+                {errors.costPrice?.currency !== undefined && (
+                  <FormErrorMessage fontSize="sm">
+                    {errors.costPrice.currency.message}
+                  </FormErrorMessage>
+                )}
+                {errors.costPrice?.amount !== undefined &&
+                  !errors.costPrice?.currency && (
+                    <FormErrorMessage fontSize="sm">
+                      {errors.costPrice.amount.message}
+                    </FormErrorMessage>
+                  )}
+              </ModalFormControl>
+
+              <ModalFormControl
+                isInvalid={errors.expectedArrivalDate !== undefined}
+              >
+                <FormLabel fontSize="sm">Expected arrival date</FormLabel>
+                <Input
+                  {...register("expectedArrivalDate", {
+                    required: "Arrival date is required",
+                    validate: (date: string) =>
+                      dayjs(date).isSameOrAfter(dayjs().startOf("day"))
+                        ? undefined
+                        : "Arrival date cannot be in the past",
+                  })}
+                  fontSize="sm"
+                  min={dayjs(new Date()).format("YYYY-MM-DD")} // make min today
+                  type="date"
+                />
+                {errors.expectedArrivalDate !== undefined && (
+                  <FormErrorMessage fontSize="sm">
+                    {errors.expectedArrivalDate.message}
+                  </FormErrorMessage>
+                )}
+              </ModalFormControl>
+
+              <ModalFormControl isInvalid={errors.quantity !== undefined}>
+                <FormLabel fontSize="sm">Quantity</FormLabel>
+                <NumberInput defaultValue={1} min={1} precision={0}>
                   <NumberInputField
-                    {...register("costPrice.amount", {
-                      required: "Cost price is required",
+                    {...register("quantity", {
+                      required: "Quantity is required",
                     })}
                     fontSize="sm"
                   />
                 </NumberInput>
-              </InputGroup>
-              {errors.costPrice?.currency !== undefined && (
-                <FormErrorMessage fontSize="sm">
-                  {errors.costPrice.currency.message}
-                </FormErrorMessage>
-              )}
-              {errors.costPrice?.amount !== undefined &&
-                !errors.costPrice?.currency && (
+                {errors.quantity !== undefined && (
                   <FormErrorMessage fontSize="sm">
-                    {errors.costPrice.amount.message}
+                    {errors.quantity.message}
                   </FormErrorMessage>
                 )}
-            </ModalFormControl>
-
-            <ModalFormControl
-              isInvalid={errors.expectedArrivalDate !== undefined}
-            >
-              <FormLabel fontSize="sm">Expected arrival date</FormLabel>
-              <Input
-                {...register("expectedArrivalDate", {
-                  required: "Arrival date is required",
-                  validate: (date: string) =>
-                    dayjs(date).isSameOrAfter(dayjs().startOf("day"))
-                      ? undefined
-                      : "Arrival date cannot be in the past",
-                })}
-                fontSize="sm"
-                min={dayjs(new Date()).format("YYYY-MM-DD")} // make min today
-                type="date"
-              />
-              {errors.expectedArrivalDate !== undefined && (
-                <FormErrorMessage fontSize="sm">
-                  {errors.expectedArrivalDate.message}
-                </FormErrorMessage>
-              )}
-            </ModalFormControl>
-
-            <ModalFormControl isInvalid={errors.quantity !== undefined}>
-              <FormLabel fontSize="sm">Quantity</FormLabel>
-              <NumberInput defaultValue={1} min={1} precision={0}>
-                <NumberInputField
-                  {...register("quantity", {
-                    required: "Quantity is required",
-                  })}
-                  fontSize="sm"
-                />
-              </NumberInput>
-              {errors.quantity !== undefined && (
-                <FormErrorMessage fontSize="sm">
-                  {errors.quantity.message}
-                </FormErrorMessage>
-              )}
-            </ModalFormControl>
-          </ModalBody>
-          <ModalFooter>
-            <ModalFooterButton colorScheme="blue" type="submit">
-              Add
-            </ModalFooterButton>
-            <ModalFooterButton
-              colorScheme="blue"
-              onClick={closeModal}
-              variant="outline"
-            >
-              Cancel
-            </ModalFooterButton>
-          </ModalFooter>
-        </ModalContent>
-      </form>
-    </Modal>
+              </ModalFormControl>
+            </ModalBody>
+            <ModalFooter>
+              <ModalFooterButton colorScheme="blue" type="submit">
+                Add
+              </ModalFooterButton>
+              <ModalFooterButton
+                colorScheme="blue"
+                onClick={closeModal}
+                variant="outline"
+              >
+                Cancel
+              </ModalFooterButton>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+      <ConfirmCancelChangesModal
+        isOpen={isConfirmCancelModalOpen}
+        onClose={closeConfirmCancel}
+        onConfirm={onClose}
+      />
+    </>
   );
 };
 
