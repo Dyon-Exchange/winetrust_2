5c2e22b2e63349e9d9283cb1cff90c8449c71cab
Max Taylor <max@labrys.io>
2021-10-12 14:29:02 +1000

    Fixed comments

--- packages/react-app/src/api/preAdvice/getPreAdvices.ts
+++ packages/react-app/src/api/preAdvice/getPreAdvices.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default async (): Promise<PreAdvice[]> => {
-  const response = await axios.get("/pre-advice");
-  const { data } = response;
+  const { data } = await axios.get("/pre-advice");
   return data;
 };
