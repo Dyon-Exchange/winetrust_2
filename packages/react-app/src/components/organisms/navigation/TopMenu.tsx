1daf9dd7b678f40e5407449d67f1a14090870821
Max Taylor <max@labrys.io>
2021-10-12 14:01:24 +1000

    Finished log out

--- packages/react-app/src/components/organisms/navigation/TopMenu.tsx
+++ packages/react-app/src/components/organisms/navigation/TopMenu.tsx
@@ -1,18 +1,31 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 import React, { useContext } from "react";
 
 import { AuthContext } from "../../../contexts/AuthContext";
 
+const TopButton = ({ email }: { email: string }) => (
+  <Button rightIcon={<ChevronDownIcon />}>
+    <Text>{email}</Text>
+  </Button>
+);
+
 const TopMenu = () => {
   const { logout, loggedIn, authDetails } = useContext(AuthContext);
 
-  if (!loggedIn) return null;
+  if (!loggedIn || !authDetails) return null;
 
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        {authDetails?.email}
+      <MenuButton>
+        <TopButton email={authDetails?.email} />
       </MenuButton>
       <MenuList>
         <MenuItem onClick={logout}>Logout</MenuItem>
