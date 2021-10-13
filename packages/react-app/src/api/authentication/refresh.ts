86cc735f13bbffa8e277ca7159c48e416965108d
Steven Balagtas <steven@labrys.io>
2021-10-13 10:17:27 +1000

    refactor: refactored login and refresh route to use the login data interface

--- packages/react-app/src/api/authentication/refresh.ts
+++ packages/react-app/src/api/authentication/refresh.ts
@@ -2,13 +2,9 @@ import axios from "axios";
 
 import { LoginData } from "./login";
 
-export default async (
-  refreshTokenParam: string
-): Promise<{ token: string; refreshToken: string }> => {
-  const response = await axios.post("/admins/refresh", {
+export default async (refreshTokenParam: string): Promise<LoginData> => {
+  const { data } = await axios.post("/admins/refresh", {
     refreshToken: refreshTokenParam,
   });
-  const { token, refreshToken } = response.data as LoginData;
-
-  return { token, refreshToken };
+  return data;
 };
