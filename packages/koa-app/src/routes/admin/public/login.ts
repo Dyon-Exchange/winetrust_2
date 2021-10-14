cb6dc0cf4dd70935bbea082f556e22859292e037
Max Taylor <max@labrys.io>
2021-10-14 11:48:52 +1000

    Fixed up expiry

--- packages/koa-app/src/routes/admin/public/login.ts
+++ packages/koa-app/src/routes/admin/public/login.ts
@@ -28,8 +28,8 @@ export default async (ctx: ExtendedContext<LoginRequest>) => {
   const payload = {
     id: account._id,
   };
-  // { expiresIn: 36000 }
-  const token = sign(payload, process.env.TOKEN_SECRET, { expiresIn: 5 });
+
+  const token = sign(payload, process.env.TOKEN_SECRET, { expiresIn: 36000 });
   const refreshToken = sign(payload, process.env.REFRESH_SECRET, {
     expiresIn: "7d",
   });
