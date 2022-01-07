58ecf42ad9c5bdce3f39fdf0accc69ed6b2abded
virlanchainworks <virlanchainworks@gmail.com>
2022-01-07 20:39:19 +0800

    removed the background coloring for StyledDataGrid

--- packages/react-app/src/components/atoms/tables/StyledDataGrid.tsx
+++ packages/react-app/src/components/atoms/tables/StyledDataGrid.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 // min height and max height takes into account the top bar
 const StyledDataGrid = styled(DataGrid)({
-  background: "grey",
   border: "none",
   minHeight: "225px",
   height: "calc(100vh - 300px)",
