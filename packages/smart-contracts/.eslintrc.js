1e51e5ac38a3cf964ce66e5e73bbdb1de1e2c980
chychkoi <chychkoi@gmail.com>
2022-01-22 11:27:08 +0200

    Update dependencies and add assign role script

--- packages/smart-contracts/.eslintrc.js
+++ packages/smart-contracts/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: "eslint-config-labrys/base",
   parser: "@typescript-eslint/parser",
+  rules: {
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
+  }
 };
