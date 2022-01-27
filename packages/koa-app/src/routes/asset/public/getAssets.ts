edc41d785f2b40c7ad95be513b64e69d1ee2b97b
Adace <sequena.alex@gmail.com>
2022-01-27 22:05:43 +0800

    update

--- packages/koa-app/src/routes/asset/public/getAssets.ts
+++ packages/koa-app/src/routes/asset/public/getAssets.ts
@@ -1,24 +1,24 @@
 import { mongoose } from "@typegoose/typegoose";
 import { Context } from "koa";
-import { toInteger } from "lodash";
+import { isInteger, toInteger } from "lodash";
 
 // import asset from "..";
 import Asset from "../../../models/Asset";
 
 export default async (ctx: Context) => {
   // ctx.body = ctx.request.params
-  const {type} = ctx.request.params;
-  const {id} = ctx.request.params;
+  const {type,searchtext} = ctx.request['params'];
   let assets: any;
   if (type === "token") {
-    assets = await Asset.find({ tokenId: id });
+    const tokenId : number = +searchtext
+    assets = await Asset.find({ tokenId: tokenId });
     ctx.body = assets;
   }
   if (type === "product") {
-    assets = await Asset.find({ productname: id }).populate({
+    assets = await Asset.find({ productname: searchtext }).populate({
       path: "product",
       match: {
-        productName:id
+        productName:searchtext
       }
     });
     ctx.body = assets.filter((asset) => asset.product);
