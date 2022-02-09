2d512474c59325d8a5c2fe5f67c132be897683c7
Anju <anju.joshi@cruworldwine.com>
2022-02-09 10:55:45 +0530

    changed favicon, logo, menu header ui fix

--- packages/react-app/src/components/atoms/tables/StyledDataGrid.tsx
+++ packages/react-app/src/components/atoms/tables/StyledDataGrid.tsx
@@ -3,10 +3,17 @@ import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 
 // min height and max height takes into account the top bar
+
 const StyledDataGrid = styled(DataGrid)({
   border: "none",
   minHeight: "225px",
   height: "calc(100vh - 300px)",
+  "& .MuiDataGrid-columnsContainer" : {
+    background: "rgba(43, 108, 176, 0.5)"
+  },
+  "& .MuiDataGrid-columnHeaderTitleContainer" : {
+    color: "#fff"
+  }
 });
 
 export default StyledDataGrid;
