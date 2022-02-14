a2107d817186d631620cc6a082b8b2523092796d
virlanchainworks <virlanchainworks@gmail.com>
2022-02-14 08:46:51 +0800

    saving

--- packages/koa-app/src/routes/preAdvice/private/getPreAdvices.ts
+++ packages/koa-app/src/routes/preAdvice/private/getPreAdvices.ts
@@ -4,6 +4,6 @@ import PreAdvice from "../../../models/PreAdvice";
 
 export default async (ctx: Context) => {
   ctx.body = await PreAdvice.find().populate(
-    "owner transferringWarehouse arrivalWarehouse"
+    "owner transferringWarehouse arrivalWarehouse firstName lastName"
   );
 };
